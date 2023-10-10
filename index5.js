d565e4c5fc30c88a78c182274497be240d440b5d
Romana2321 <147380259+Romana2321@users.noreply.github.com>
2023-10-10 16:07:22 +0500

    Add files via upload

---
+++ index5.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let Famous_Person;
+Famous_Person = 'Albert Einstein Ones said';
+let Quote1;
+Quote1 = 'A person who never made a mistake never tried anything new';
+const message = (Famous_Person + "," + " \" " + Quote1 + ". \" ");
+console.log(message);
